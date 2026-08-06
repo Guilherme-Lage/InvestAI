d81e1ec37434bffc8c72e6a4b319061652741ef5
Guilherme-Casa <guilhermelagec762@gmail.com>
2026-08-06 20:11:03 -0300

    commit4

--- investai/investai/backend/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .usuario_controller import usuario_bp
-from .movimentacao_controller import movimentacao_bp
-from .investimento_controller import investimento_bp
-from .meta_controller import meta_bp
-
-__all__ = ["usuario_bp", "movimentacao_bp", "investimento_bp", "meta_bp"]
